4e12b8582cff4bc0caf30abc78486d431b316595
Bachbeos <trinhbachtt@gmail.com>
2024-09-20 21:41:19 +0700

    manpage

---
+++ front_end/main.js
@@ -0,0 +1,8 @@
+// function setScale() {
+//     let zoom = window.innerWidth / 1920;
+//     document.body.style.zoom = zoom
+// }
+// setScale();
+// document.addEventListener("DOMContentLoaded", setScale);
+// window.addEventListener("resize", setScale);
+// window.addEventListener("load", setScale);
